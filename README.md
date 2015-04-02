1db3c98de4c09208030ec3d79ea89b8e9bc6e8d4
Rafael Ricote <rafi.93.zgz@gmail.com>
2015-04-02 20:38:59 +0200

    ultimas modificaciones

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # telegram-remote
+
+Suponiendo que estas en ~/tg/ :
+
+bin/telegram-cli -k tg-server.pub -W
+Despues de instalar ejecutar y poner el numero del movil
+
+Para salir:
+
+quit
+
+Para activar el action.lua:
+bin/telegram-cli -k tg-server.pub -W -s action.lua
--- action.lua
+++ action.lua
@@ -1,38 +1,42 @@
 function on_msg_receive (msg)
-  if (msg.from.id == our_id) then
-    t={}
-    str = msg.text
-    str:gsub(".",function(c) table.insert(t,c) end)
-    if (t[1] == '$') then
-      str = string.sub(str, 2)
-      foo = os.capture(str)
-      send_msg (msg.to.print_name, foo, ok_cb, false)
-    end
+	if (msg.from.id == our_id) then
+  	--Para ejecutar comandos en la terminal
+	t={}
+	str = msg.text
+	str:gsub(".",function(c) table.insert(t,c) end)
+	if (t[1] == '$') then
+		str = string.sub(str, 2)
+		foo = os.capture(str)
+		send_msg (msg.to.print_name, foo, ok_cb, false)
+	end
+	--[[en caso de querer ejecutar un comando sin recibir
+	la salida cambiar os.capture por os.execute
+	]]
+	if (msg.text == 'ip pubica' or msg.text == 'ip1') then
+		foo = os.capture('bash /home/pi/t_notifications/ip_pubica.sh')
+		send_msg (msg.to.print_name, foo, ok_cb, false)
+	end
 
-    if (msg.text == 'ip pubica' or msg.text == 'ip1') then
-      foo = os.capture('bash /home/pi/t_notifications/ip_pubica.sh')
-      send_msg (msg.to.print_name, foo, ok_cb, false)
-    end
-    if (msg.text == 'ip2') then
-      foo = os.capture('bash /home/pi/t_notifications/ip_pubica2.sh')
-      send_msg (msg.to.print_name, foo, ok_cb, false)
-    end
-  end
-  return
+	if (msg.text == 'ip2') then
+		foo = os.capture('bash /home/pi/t_notifications/ip_pubica2.sh')
+		send_msg (msg.to.print_name, foo, ok_cb, false)
+		end
+	end
+	return
 end
 
 function os.capture(cmd, raw)
-  local f = assert(io.popen(cmd, 'r'))
-  local s = assert(f:read('*a'))
-  f:close()
-  if raw then return s end
-  s = string.gsub(s, '^%s+', '')
-  s = string.gsub(s, '%s+$', '')
-  return s
+	local f = assert(io.popen(cmd, 'r'))
+	local s = assert(f:read('*a'))
+	f:close()
+	if raw then return s end
+	s = string.gsub(s, '^%s+', '')
+	s = string.gsub(s, '%s+$', '')
+	return s
 end
 
 function on_our_id (id)
-  our_id = id
+	our_id = id
 end
 
 
